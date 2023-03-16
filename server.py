da06e4c5421c7dd5c7652f9e27c57efc156f916b
Berengar W. Lehr <Berengar.Lehr@uni-jena.de>
2023-03-16 14:49:00 +0100

    Geht soweit

--- server.py
+++ server.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-import importlib
 import logging
 import subprocess
 
 import fastapi
 import fastapi.middleware.cors
+import fastapi.responses
 import uvicorn  # type: ignore
 
 import session
@@ -18,12 +18,31 @@ app.add_middleware(
     allow_headers=['Content-Type'],
 )
 
-@app.get("/whoami")
+TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiQXNhc2VsIn0.FNVcvEC8GqY86L4TxgEQErHsqEdRsQU3aub4BAZf_0Q'
+
+@app.middleware('http')
+async def checkAuthBearerToken(
+        request: fastapi.Request,
+        call_next):
+
+    if not request.url.path.startswith('/login/') and (
+            request.headers.get('authorization') != f'Bearer {TOKEN}' and request.cookies.get('asasel_auth') != TOKEN):
+        return fastapi.responses.RedirectResponse('/login/', status_code=307)
+    return await call_next(request)
+
+@app.get('/login/{username}')
+def token(response: fastapi.Response, username: str, password: str):
+    if username == password:
+        response.set_cookie(key='asasel_auth', value=TOKEN)
+        return TOKEN
+    return ''
+
+@app.get('/whoami')
 def whoam():
     result = subprocess.run('whoami', encoding='utf8', capture_output=True)
     return (result.stdout if result.returncode == 0 else result.stderr).strip()
 
-app.mount("/session", session.app)
+app.mount('/session', session.app)
 
 if __name__ == '__main__':
     logging.basicConfig(format='%(asctime)s | %(levelname)s:     %(message)s', level=logging.INFO)
--- session.py
+++ session.py
@@ -1,32 +1,67 @@
 from fastapi import FastAPI
-import subprocess
+import lib
 
 app = FastAPI()
 
-def validUser(username: str) -> tuple[bool, str]:
-    user = username.split()[0]
-    result = subprocess.run(['id', '-u', user], encoding='utf8', capture_output=True)
-    if result.returncode != 0:
-        return False, 'user does not exist'
-    if int(result.stdout) < 1000:
-       return False, 'can not kill this user'
-    return True, user
+def endSession(username):
+    return ['killall', '-u', username, 'cinnamon-session']
 
-def shell(*cmd):
-    result = subprocess.run(cmd, encoding='utf8', capture_output=True)
-    return (result.stdout if result.returncode == 0 else f'ERROR: {result.stderr}').strip()
-
-@app.get("/killall/{username}")
+@app.get('/killall/{username}')
 def whoam(username: str):
-    ok, response = validUser(username)
+    ok, response = lib.validUser(username)
     if not ok:
         return response
-    return shell('echo', 'killall', '-u', response)
+    return lib.shell('killall', '-u', response)
 
-
-@app.get("/killsession/{username}")
+@app.get('/killsession/{username}')
 def killsession(username: str):
-    ok, response = validUser(username)
+    ok, response = lib.validUser(username)
     if not ok:
         return response
-    return shell('echo', 'pkill', '-fu', response, "'^cinnamon-session '")
+    return lib.shell(*endSession(response))
+
+def changePassword(username: str, password: str):
+    return lib.shell('passwd', username, input=f'{password}\n{password}\n')
+
+@app.get('/simplepasswd/{username}')
+def simplepasswd(username: str):
+    ok, response = lib.validUser(username)
+    if not ok:
+        return response
+    return changePassword(response, response)
+
+@app.get('/hardpasswd/{username}')
+def hardpasswd(username: str):
+    ok, response = lib.validUser(username)
+    if not ok:
+        return response
+    return changePassword(response, f'{response.capitalize()}{response.capitalize()}')
+
+@app.get('/setlimit/{username}/{time}')
+def setlimit(username: str, time: str):
+    ok, response = lib.validUser(username)
+    if not ok:
+        return response
+
+    if time[0] == '+':
+        time = f'now {time}'
+
+    return lib.shell('at', time, input=' '.join(endSession(response)))
+
+@app.get('/clearlimit/{username}')
+def clearlimit(username: str):
+    ok, response = lib.validUser(username)
+    if not ok:
+        return response
+
+    match = ' '.join(endSession(username))
+
+    for line in lib.shell('atq').split('\n'):
+        atid = line.split('\t', 1)[0]
+        command = lib.shell('at', '-c', atid).rsplit('\n', 1)[1]
+        if command == match:
+            lib.shell('atrm', atid)
+            return command
+        else:
+            print(command, match)
+    return 'No session limit found'
