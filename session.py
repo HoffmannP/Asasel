156901da649d6d7efdd60afd443569f8f47dcda5
Peter Hoffmann <Hoffmann.P@gmx.net>
2023-03-17 00:08:12 +0100

    Added getlimit

--- session.py
+++ session.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 import lib
+import datetime
 
 app = FastAPI()
 
 def endSession(username):
     return ['killall', '-u', username, 'cinnamon-session']
+
+def genHardPass(username):
+    return f'{username.capitalize()}{username.capitalize()}'
 
 @app.get('/killall/{username}')
 def whoam(username: str):
@@ -35,7 +39,7 @@ def hardpasswd(username: str):
     ok, response = lib.validUser(username)
     if not ok:
         return response
-    return changePassword(response, f'{response.capitalize()}{response.capitalize()}')
+    return changePassword(response, genHardPass(username))
 
 @app.get('/setlimit/{username}/{time}')
 def setlimit(username: str, time: str):
@@ -48,12 +52,31 @@ def setlimit(username: str, time: str):
 
     return lib.shell('at', time, input=' '.join(endSession(response)))
 
+@app.get('/getlimit/{username}')
+def getlimit(username: str):
+    ok, response = lib.validUser(username)
+    if not ok:
+        return response
+    match = ' '.join(endSession(username))
+
+    for line in lib.shell('atq').split('\n'):
+        atid = line.split('\t', 1)[0]
+        if len(line) == 0:
+            continue
+        command = lib.shell('at', '-c', atid).rsplit('\n', 1)[1]
+        if command == match:
+            then = datetime.datetime.strptime(line.rsplit('\t', 1)[1].rsplit(' ', 2)[0], '%c')
+            diff = int((then - datetime.datetime.now()).total_seconds())
+            return f'{int(diff / 3600):02d}:{int(diff / 60) % 60:02d}:{diff % 60:02d}'
+        else:
+            print(command, match)
+    return 'No session limit found'
+
 @app.get('/clearlimit/{username}')
 def clearlimit(username: str):
     ok, response = lib.validUser(username)
     if not ok:
         return response
-
     match = ' '.join(endSession(username))
 
     for line in lib.shell('atq').split('\n'):
